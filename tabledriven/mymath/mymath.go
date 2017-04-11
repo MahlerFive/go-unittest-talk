0bed5b07b93859c7eec7bf770a2ffc4c3970bffb
Jeff Armstrong <jeff@greenfieldlabs.ca>
2017-04-11 03:46:25 -0400

    remove bad version of Multiply() in tabledriven example

--- tabledriven/mymath/mymath.go
+++ tabledriven/mymath/mymath.go
@@ -1,15 +1,6 @@
 package mymath
 
-// Bad version
-func Multiply(op1 int, op2 int) int {
-	product := 0
-	for i := 0; i < op2; i++ {
-		product += op1
-	}
-	return product
-}
-
 // Good version
-// func Multiply(op1 int, op2 int) int {
-// 	return op1 * op2
-// }
+func Multiply(op1 int, op2 int) int {
+	return op1 * op2
+}
