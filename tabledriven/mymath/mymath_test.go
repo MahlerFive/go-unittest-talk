d40433cebf17914c4fe4333b28ab24e22d265f8d
Jeff Armstrong <jeff@greenfieldlabs.ca>
2017-04-10 23:14:26 -0400

    code examples for table driven tests

---
+++ tabledriven/mymath/mymath_test.go
@@ -0,0 +1,56 @@
+package mymath_test
+
+import (
+	"testing"
+	"unit-test-talk/tabledriven/mymath"
+)
+
+func TestMultiply(t *testing.T) {
+	type TestCase struct {
+		Name            string
+		Op1             int
+		Op2             int
+		ExpectedProduct int
+	}
+
+	testCases := []TestCase{
+		{
+			Name:            "multiply by 0",
+			Op1:             0,
+			Op2:             5,
+			ExpectedProduct: 0,
+		},
+		{
+			Name:            "multiply positives",
+			Op1:             3,
+			Op2:             5,
+			ExpectedProduct: 15,
+		},
+		{
+			Name:            "multiply negative by positive",
+			Op1:             -3,
+			Op2:             5,
+			ExpectedProduct: -15,
+		},
+		{
+			Name:            "multiply positive by negative",
+			Op1:             3,
+			Op2:             -5,
+			ExpectedProduct: -15,
+		},
+		{
+			Name:            "multiply negatives",
+			Op1:             -3,
+			Op2:             -5,
+			ExpectedProduct: 15,
+		},
+	}
+
+	for _, tc := range testCases {
+		actualProduct := mymath.Multiply(tc.Op1, tc.Op2)
+		if actualProduct != tc.ExpectedProduct {
+			t.Errorf("%s: %d * %d = %d, expected %d", tc.Name, tc.Op1, tc.Op2, actualProduct, tc.ExpectedProduct)
+		}
+	}
+
+}
