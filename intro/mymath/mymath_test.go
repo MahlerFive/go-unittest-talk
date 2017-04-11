2152428389884a4cb258dcb6e6fd1d46dfbef53d
Jeff Armstrong <jeff@greenfieldlabs.ca>
2017-04-10 23:14:10 -0400

    code examples for intro

---
+++ intro/mymath/mymath_test.go
@@ -0,0 +1,48 @@
+package mymath_test
+
+import (
+	"testing"
+	"unit-test-talk/intro/mymath"
+)
+
+func TestMultiply(t *testing.T) {
+	op1 := 0
+	op2 := 5
+	expectedProduct := 0
+	actualProduct := mymath.Multiply(op1, op2)
+	if actualProduct != expectedProduct {
+		t.Errorf("%d * %d = %d, expected %d", op1, op2, actualProduct, expectedProduct)
+	}
+
+	op1 = 3
+	op2 = 5
+	expectedProduct = 15
+	actualProduct = mymath.Multiply(op1, op2)
+	if actualProduct != expectedProduct {
+		t.Errorf("%d * %d = %d, expected %d", op1, op2, actualProduct, expectedProduct)
+	}
+
+	op1 = -3
+	op2 = 5
+	expectedProduct = -15
+	actualProduct = mymath.Multiply(op1, op2)
+	if actualProduct != expectedProduct {
+		t.Errorf("%d * %d = %d, expected %d", op1, op2, actualProduct, expectedProduct)
+	}
+
+	op1 = 3
+	op2 = -5
+	expectedProduct = -15
+	actualProduct = mymath.Multiply(op1, op2)
+	if actualProduct != expectedProduct {
+		t.Errorf("%d * %d = %d, expected %d", op1, op2, actualProduct, expectedProduct)
+	}
+
+	op1 = -3
+	op2 = -5
+	expectedProduct = 15
+	actualProduct = mymath.Multiply(op1, op2)
+	if actualProduct != expectedProduct {
+		t.Errorf("%d * %d = %d, expected %d", op1, op2, actualProduct, expectedProduct)
+	}
+}
